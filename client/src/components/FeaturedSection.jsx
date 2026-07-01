d4b0acaaf55a8e8248814413474d29e52afb72f0
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-02 01:01:31 +0530

    BlueCircle complonent created styled and proped dynamicaly to position the decorative item

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,6 +1,7 @@
 import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import BlurCircle from './BlurCircle'
 
 const FeaturedSection = () => {
 
@@ -10,16 +11,19 @@ const FeaturedSection = () => {
         <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
 
             <div className='relative flex items-center justify-between pt-20 pb-10'>
+                <BlurCircle top="0" right="-80px" />
                 <p className='text-gray-300 font-medium text-lg'>Now Showing</p>
-                <button onClick={() => navigate("/movies")} className='group flex items-center gap-2 text-sm text-gray-300'>
+                <button onClick={ () => {navigate("/movies"); scrollTo(0,0)} } className='group flex items-center gap-2 text-sm text-gray-300 cursor-pointer hover:text-[16px] transition-all duration-300'>
                     View All
-                    <ArrowRight className='group-hover:translate-x-0.5 transition w-4.5 h-4.5' />
+                    <ArrowRight className='group-hover:translate-x-0.5 transition-all w-4.5 group-hover:text-lg h-4.5 duration-300' />
                 </button>
             </div>
 
             <div></div>
 
-            <div></div>
+            <div className='flex justify-center mt-20'>
+                <button onClick={ () => {navigate("/movies"); scrollTo(0,0)} } className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Explore More</button>
+            </div>
 
         </div>
     )
