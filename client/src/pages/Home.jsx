d57428b483197021928f4315b54dddc8c931c500
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-01 23:57:37 +0530

    Header Section Created for the Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
 
-  return (
-    <div>
-              
-    </div>
-  )
+    return (
+        <>
+            <HeroSection />
+        </>
+    )
 
 }
 
