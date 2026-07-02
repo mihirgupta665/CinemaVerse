35403dd928d433f74c14bff415ee72e0a37e5be7
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-02 16:09:17 +0530

    MovieDetails page created successfully

--- client/src/components/BlurCircle.jsx
+++ client/src/components/BlurCircle.jsx
@@ -4,7 +4,7 @@ const BlurCircle = ({ top = "auto", left = "auto", right = "auto", bottom = "aut
     return (
         <div className="absolute -z-50 h-58 w-58 aspect-square rounded-full bg-primary/30 blur-3xl" 
         style={{top:top, left:left, right:right, bottom:bottom}}>
-            BlurCircle
+            
         </div>
     )
 }
