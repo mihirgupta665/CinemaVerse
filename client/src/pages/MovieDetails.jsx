24f026e852721d0978b0a2c129551daf9bfd0017
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-02 22:50:09 +0530

    movie details page half created

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import BlurCircle from '../components/BlurCircle'
-import { StarIcon } from "lucide-react"
+import { Heart, PlayCircleIcon, StarIcon } from "lucide-react"
 import timeFormat from '../lib/timeFormat'
 
 const MovieDetails = () => {
@@ -37,7 +37,7 @@ const MovieDetails = () => {
                         <h1 className="text-4xl font-semibold max-w-96 text-balance">{show.movie.title}</h1>
 
                         <div className='flex items-center gap-2 text-gray-300'>
-                            <StarIcon className='w-5 h-5 text-primary fill-primary'/>
+                            <StarIcon className='w-5 h-5 text-primary fill-primary' />
                             {show.movie.vote_average.toFixed(1)} User Rating
                         </div>
 
@@ -47,12 +47,35 @@ const MovieDetails = () => {
                             {timeFormat(show.movie.runtime)} • {show.movie.genres.map(genre => genre.name).join(", ")} • {show.movie.release_date.split("-")[0]}
                         </p>
 
+                        <div className='flex items-center flex-wrap gap-4 mt-4'>
+                            <button className='flex items-center gap-2 px-7 py-3 text-sm bg-gray-800 hover:bg-gray-900 transition rounded-md font-medium cursor-pointer active:scale-95'>
+                                <PlayCircleIcon className='w-5 h-5' />
+                                Watch Trailer
+                            </button>
+                            <a href="#dateSelect" className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95'>Buy Tickets</a>
+                            <button className='bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
+                                <Heart className={`w-5 h-5`} />
+                            </button>
+                        </div>
+
                     </div>
 
                 </div>
 
+                <p className='text-lg font-medium mt-20'>Your Favorite Cast</p>
+                <div className='overflow-x-auto no-scrollbar mt-8 pb-4'>
+                    <div className='flex items-center gap-4 w-max px-4'>
+                        {show.movie.casts.slice(0,12).map( (cast, index) => (
+                            <div key={index} className='flex flex-col items-center text-center'>
+                                <img src={cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover'/>
+                                <p>{cast.name}</p>
+                            </div>
+                        ))  }
+                    </div>
+                </div>
+
             </div>
-        ) 
+        )
         : (
             <div>
                 Loading...
