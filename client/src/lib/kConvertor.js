f6fbca3cab3aab19a6f961ace8111c6f1060f9ac
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-05 00:27:27 +0530

    KConvertor created to convert vote count and Add show page functionlity created to handle the date time picker selection with help of states

---
+++ client/src/lib/kConvertor.js
@@ -0,0 +1,10 @@
+// K Convertor
+
+export const kConvertor = (num) => {
+    if(num >= 1000){
+        return (num/1000).toFixed(1) + "K"
+    }
+    else{
+        return num
+    }
+}
--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
+import { CheckIcon, StarIcon } from "lucide-react"
+import { kConvertor } from '../../lib/kConvertor';
 
 const AddShows = () => {
 
@@ -21,18 +23,40 @@ const AddShows = () => {
         fetchNowPlayingMovies();
     }, []);
 
-    return nowPlayingMovies > 0
+    return nowPlayingMovies.length > 0
         ? (
             <>
                 <Title text1="Add" text2="Shows" />
                 <p className='mt-10 text-lg font-medium'>Now Playing Movies</p>
-                <div className='overflow-x-auto pb-4'>
-                    <div className='group flex flex-wrap gap-4 mt-4 w-max'>
+                <div className='overflow-x-auto pb-4 no-scrollbar'>
+                {/* <div className='overflow-x-auto pb-4'> */}
+                    <div className='group flex flex-wrap gap-4 mt-4 w-max '>
                         {nowPlayingMovies.map((movie) => (
-                            <div key={movie.id} className={`relative max-w-40 cursor-pointer group-hover:not-hover:opacity-40 hover:-translate-y-1 transition duration-300`}></div>
+                            <div key={movie.id} onClick={() => setSelectedMovie(movie.id)} className={`relative max-w-40 cursor-pointer group-hover:not-hover:opacity-60 hover:-translate-y-1 transition duration-300`}>
+                                <div className='relative rounded-lg overflow-hidden'>
+                                    <img src={movie.poster_path} alt="" className='w-full object-cover brightness-90' />
+                                    <div className="text-sm flex items-center justify-between p-2 bg-black/70 w-full absolute bottom-0 left-0">
+                                        <p className="flex items-center gap-1 text-gray-400">
+                                            <StarIcon className="w-4 h-4 text-primary fill-primary" />
+                                            {movie.vote_average.toFixed(1)}
+                                        </p>
+                                        <p className="text-gray-300">{kConvertor(movie.vote_count) } Votes</p>
+                                    </div>
+                                </div>
+                                {selectedMovie === movie.id && (
+                                    <div className="absolute top-2 right-2 flex items-center justify-center bg-primary h-6 w-6 rounded">
+                                        <CheckIcon className="w-4 h-4 text-white" strokeWidth= {2.5} />
+                                    </div>
+                                )}
+                                <p className='font-medium truncate'>{movie.title}</p>
+                                <p className='text-gray-400 text-sm'>{movie.release_date}</p>
+                            </div>
                         ))}
                     </div>
                 </div>
+
+                {/* Shows Price Input */}
+
             </>
         )
         : <Loading />
