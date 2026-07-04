74416efe2acb134f1bc5f8bdbc51c7f86a663e7d
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-04 20:33:40 +0530

    createdList  booking page with full responsiveness and all the functionality

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const AddShows = () => {
-  return (
-    <div>AddShows</div>
-  )
+
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [dateTimeSelection, setDateTimeSelection] = useState({});
+    const [dateTimeInput, setDateTimeInput] = useState("");
+    const [showPrice, setShowPrice] = useState("");
+
+    return (
+        <div>AddShows</div>
+    )
 }
 
 export default AddShows
