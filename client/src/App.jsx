9e4cadb88563e469cc9dc9f49b204779d4b48c94
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-04 14:00:02 +0530

    Navbar completly structured and layout made with responsiveness

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,11 +15,13 @@ import Dashboard from './pages/admin/Dashboard'
 import AddShows from './pages/admin/AddShows'
 import ListShows from './pages/admin/ListShows'
 import ListBookings from './pages/admin/ListBookings'
+import { assets } from './assets/assets'
 
 const App = () => {
 
     const isAdminRoute = useLocation().pathname.startsWith("/admin")
     // console.log(isAdminRoute)
+
 
     return (
 
@@ -28,17 +30,19 @@ const App = () => {
             {!isAdminRoute && <Navbar />}
             <Routes>
 
-                <Route path="/" element={ <Home /> } />
-                <Route path="/movies" element={ <Movies /> } />
-                <Route path="/movies/:id" element={ <MovieDetails /> } />
-                <Route path="/movies/:id/:date" element={ <SeatLayout /> } />
-                <Route path="/my-bookings" element={ <MyBookings /> } />
-                <Route path="/favorite" element={ <Favorite /> } />
+                <Route path="/" element={<Home />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/movies/:id" element={<MovieDetails />} />
+                <Route path="/movies/:id/:date" element={<SeatLayout />} />
+                <Route path="/my-bookings" element={<MyBookings />} />
+                <Route path="/favorite" element={<Favorite />} />
 
-                <Route path="/admin/*" element={<Layout /> }> 
+                <Route path="/admin/*" element={<Layout />}>
                     <Route index element={<Dashboard />} />
-                    <Route path="add-shows" element={<AddShows />}  />
-                    <Route path="list-bookings" element={<ListBookings />}  />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="add-shows" element={<AddShows />} />
+                    <Route path="list-shows" element={<ListShows />} />
+                    <Route path="list-bookings" element={<ListBookings />} />
                 </Route>
 
             </Routes>
--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import { assets } from '../../assets/assets'
 
 const AdminNavbar = () => {
   return (
-    <div>
-        
+      <div className='flex items-center justify-between px-6 pt-2 md:px-10 h-auto border-b border-gray-300/30'>
+        <Link to="/admin">
+            <img src={assets.logo} alt="Admin Logo" className='w-36 h-auto'/>
+        </Link>
     </div>
   )
 }
