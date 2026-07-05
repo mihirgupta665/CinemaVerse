9b36a75c1438538f3397c1c6ffb188edd14a4b6c
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-05 23:41:12 +0530

    updating the versionso that inter between dependencies become compatible with each other

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -13,7 +13,7 @@ const syncUserCreation = inngest.createFunction(
 
     async ({ event }) => {
         const {id, first_name, last_name, email_addresses, image_url} = event.data
-        const  {userData} = {
+        const  userData = {
             _id:id, 
             email: email_addresses[0].email_address,
             name: first_name + " " + last_name,
