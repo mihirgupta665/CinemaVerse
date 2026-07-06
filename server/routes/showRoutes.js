e173f8b259f575ab9f72e80803daf9df560bd896
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-07 00:03:33 +0530

    Protected admin route with the admin middlewares to get the privatemetada data and verifying it for admin

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { addShow, getNowPlayingMovies } from "../controllers/showControllers.js"
+import { protectAdmin } from "../middlewares/auth.js"
 
 const showRouter = express.Router()
 
-showRouter.get("/now-playing", getNowPlayingMovies)
-showRouter.post("/add", addShow)
+showRouter.get("/now-playing", protectAdmin, getNowPlayingMovies)
+showRouter.post("/add", protectAdmin, addShow)
  
 
 
