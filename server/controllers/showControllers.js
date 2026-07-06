62e7eafc339c6d86cab92c27ca3a490e4ff14778
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-06 15:13:51 +0530

    api call made to tmdb databse to get the movie with id and the movie cast with help of id and process the data

--- server/controllers/showControllers.js
+++ server/controllers/showControllers.js
@@ -1,18 +1,72 @@
 import axios from "axios"
+import Movie from "../models/Movie.js";
 
-export const getNowPlayingMovies = async(req, res) => {
+// API to get now playing movies from TMDB API
+export const getNowPlayingMovies = async (req, res) => {
     try {
-        
-        const {data} = await axios.get("https://api.themoviedb.org/3/movie/now_playing", {
-            headers: {Authorization: `Bearer ${process.env.TMDB_API_KEY}`}
+
+        const { data } = await axios.get("https://api.themoviedb.org/3/movie/now_playing", {
+            headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` }
         })
 
         const movies = data.results;
-        res.json({success: true, movies : movies})
+        res.json({ success: true, movies: movies })
 
     }
     catch (error) {
-        console.log("Error occured during the api call for fetching movies from the TMDB database. Error : ",error)
-        res.json({success: false, message: error.message})
+        console.log("Error occured during the api call for fetching movies from the TMDB database. Error : ", error)
+        res.json({ success: false, message: error.message })
     }
+}
+
+// API to add a new show to the database
+export const addShow = async (req, res) => {
+
+    try {
+
+        const { movieId, showsInput, showPrice } = req.body
+
+        let movie = await Movie.findById(movieId)
+
+        if (!movie) {
+            // fetch movie details and credits from the TMDB API
+            const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}`,
+                    { headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` } }),
+
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, 
+                    { headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` } })
+                
+            ]);
+
+            const movieApiData = movieDetailsResponse.data;
+            const movieCreditsData = movieCreditsResponse.data;
+            console.log("Movie Credits Data of response : ", movieCreditsResponse)
+
+            const movieDetails = {
+                _id: movieId,
+                title: movieApiData.title,
+                overview: movieApiData.overview,
+                poster_path: movieApiData.poster_path,
+                backdrop_path: movieApiData.backdrop_path,
+                genres: movieApiData.genres,
+                casts: movieCreditsData.cast,
+                release_data: movieApiData.release_date,
+                original_language: movieApiData.original_language,
+                tagline: movieApiData.tagline || "",
+                vote_average: movieApiData.vote_average,
+                runtime: movieApiData.runtime,
+            }
+
+            const moviedoc = await Movie.create(movieDetails)
+
+        }
+
+    }
+    catch (error) {
+        console.log("Error occured during the Error : ", error)
+        res.json({ success: false, message: error.message })
+    }
+
 }
