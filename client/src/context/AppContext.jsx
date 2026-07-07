93a7addde9b9881a475990094cee00016473c77d
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-08 02:26:02 +0530

    Context api create for the application to take the global states and function and access it globally

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,21 @@
-import { useContext } from "react";
-import { createContext } from "react";
+import { useState, useContext, createContext } from "react";
+import axios from "axios"
 
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL  
 
 export const AppContext = createContext()
 
 export const AppProvider = ({children}) => {
 
-    const value = {}
+    const [isAdmin, setIsAdmin] = useState(false)
+    const [shows, setShows] = useState([])
+    const [favoriteMovies, setFavoriteMovies] = useState([])
+
+    const value = {
+        axios,
+        
+    }
+    useEffect
 
     return (
         <AppContext.Provider value={value}>
