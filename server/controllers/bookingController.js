83c78dd157d6f95407509a6a69267a1f3316328a
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-07 12:10:45 +0530

    Controller function created to book a show for a certain time and update the smae in booking database and occupied seat for the show

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,3 +1,4 @@
+import Booking from "../models/Booking.js";
 import Show from "../models/Show.js"
 
 
@@ -28,12 +29,46 @@ export const createBooking = async (req, res) => {
 
     try {
         
-        
+        const {userId} = req.auth();
+        const {showId, selectedSeats} = req.body
+        const { origin } = req.headers; 
 
+        // Checign seat availability for the selected show and selected seats
+        const isAvailable = await checkSeatAvailability(showId, selectedSeats)
+
+        if(!isAvailable){
+            return res.json({success:false, message: "Selected seats are not available."})
+        }
+
+        // Get the show details
+        const showData = await Show.findById(showId).populate("movie");
+
+        // Create a new booking
+        const booking = await Booking.create({
+            user: userId,
+            show: showId,
+            amount: showData.showPrice * selectedSeats.length,
+            bookedSeats: selectedSeats
+        })
+
+        // reserving the newly selected seats in the shows database 
+        selectedSeats.map((seat) => {
+            showData.occupiedSeats[seat] = userId
+        })
+
+        showData.markModified("occupiedSeats")
+
+        await showData.save();
+
+        // Stripe Gateway Initialize
+
+        res.json({success: true, message: "Booked Successfully"})
 
     }
     catch (error) {
-        
+        console.log("Error occured during creation of booking data and reserving the seats in the show database.")
+        res.json({success: false, message:error.message})
     }
-
 }
+
+ 
