578bee1bde02264239c9d1f9f0aed2e7fcda5d66
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-08 02:13:05 +0530

    App context api created for application

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+
+export const AppContext = createContext()
+
+export const AppProvider = ({children}) => {
+
+    const value = {}
+
+    return (
+        <AppContext.Provider value={value}>
+            { children }
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => useContext(AppContext)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,20 +4,21 @@ import App from './App.jsx'
 
 import { BrowserRouter } from "react-router-dom"
 import { ClerkProvider } from "@clerk/clerk-react"
+import { AppProvider } from './context/AppContext.jsx'
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+    throw new Error('Missing Publishable Key')
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-
-  </ClerkProvider>,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <BrowserRouter>
+            <AppProvider>
+                <App />
+            </AppProvider>
+        </BrowserRouter>
+    </ClerkProvider>,
 )
