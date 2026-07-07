dc01a63de9d5d411aa1698b1791a557ea7d86f80
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-07 16:44:27 +0530

    Admin Router and shows route created in main server

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,5 +1,41 @@
+import Booking from "../models/Booking"
+import Show from "../models/Show";
+import User from "../models/User";
 
 // Controller function to check if user is Admin
 export const isAdmin = async (req, res) => {
-    res.json({success:true, isAdmin: true})
+    res.json({ success: true, isAdmin: true })
+}
+
+// API to get dashboard data
+export const getDashboardData = async (req, res) => {
+    try {
+
+        const bookings = await Booking.find({ isPaid: true });
+
+        const activeShows = await Show.find({ showDateTime: { $gte: new Date() } }).populate("movie")
+
+        const totalUser = await User.countDocuments();
+
+        /*
+        // use promiseAll
+        const { bookings, activeShows, totalUser } = Promise.all([
+            Booking.find({ isPaid: true }), 
+            Show.find({ showDateTime: { $gte: new Date() } }).populate("movie"),
+            User.countDocuments()
+        ])
+        */
+
+        const dashboardData = {
+            totalBookings: bookings.length,
+            totalRevenue: bookings.reduce((acc, booking) => acc + booking.amount, 0),
+            activeShows,
+            totalUser
+        }
+
+
+    }
+    catch (error) {
+
+    }
 }
