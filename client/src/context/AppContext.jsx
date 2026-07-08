4da964fc9b31e5def62305b5482879f3e4916994
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-08 11:17:51 +0530

    Frontent function created to get the authorization of admin and do the needfull

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,25 +1,80 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 import axios from "axios"
+import { useAuth, useUser } from "@clerk/clerk-react"
+import { useLocation, useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL  
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 
 export const AppContext = createContext()
 
-export const AppProvider = ({children}) => {
+export const AppProvider = ({ children }) => {
+
+    const { user } = useUser()
+    const { getToken } = useAuth()
+    const location = useLocation()
+
+    const navigate = useNavigate()
 
     const [isAdmin, setIsAdmin] = useState(false)
     const [shows, setShows] = useState([])
     const [favoriteMovies, setFavoriteMovies] = useState([])
 
+
+    const fetchIsAdmin = async () => {
+
+        try {
+
+            const { data } = axios.get("/api/admin/is-admin", { headers: { Authorization: `Bearer ${await getToken()}` } })
+            setIsAdmin(data.isAdmin)
+
+            if(!data.isAdmin && location.pathname.startsWith("/admin") ){
+                toast.warn("You Are not Authorized as Admin!")
+                navigate("/")
+            }
+
+        }
+        catch(error) {
+            console.log("Error Occured while fetching the user admin authorization from backend and clerk. Error : ",error)
+        }
+    }
+
+    useEffect(() => {
+        if(user){
+            fetchIsAdmin()
+        }
+    }, [user])
+
+
+
+    const fetchShows = async () => {
+        try {
+        
+            const {data} = await axios.get("/api/admin/all-shows")
+            if(data.success){
+                setShows(data.shows)
+            }
+
+        } 
+        catch(error) {
+
+        }
+    }
+
+
+
     const value = {
         axios,
-        
+        isAdmin, setIsAdmin,
+        shows, setShows,
+        favoriteMovies, setFavoriteMovies,
+
+
     }
-    useEffect
 
     return (
         <AppContext.Provider value={value}>
-            { children }
+            {children}
         </AppContext.Provider>
     )
 }
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,23 +1,23 @@
-import {clerkClient} from "@clerk/express";
+import { clerkClient } from "@clerk/express";
 
 export const protectAdmin = async (req, res, next) => {
     try {
 
-        const {userId} = req.auth();
-        if(!userId){
-            return res.json({success:false, message:"Not Authorized! Login Again!!!"})
+        const { userId } = req.auth();
+        if (!userId) {
+            return res.json({ success: false, message: "Not Authorized! Login Again!!!" })
         }
 
         const user = await clerkClient.users.getUser(userId)
 
-        if(user.privateMetadata.role !== "admin"){
-            return res.json({success: false, message: "Not Authorized as Admin!" })
+        if (user.privateMetadata.role !== "admin") {
+            return res.json({ success: false, message: "Not Authorized as Admin!" })
         }
 
         next();
     }
     catch (error) {
         console.log("Error occured during fetching the user metadata from clerk. Error : ", error)
-        res.json({success:false, message:error.message })
+        res.json({ success: false, message: error.message })
     }
 }
