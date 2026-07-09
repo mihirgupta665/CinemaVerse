d5963b967bc492ebf22cb66d4c4f8f5e69cc1f10
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-09 13:38:36 +0530

    My Bookings  page transformed to dynamic by displaying the user related data and functionly with help of backend and then database

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -4,40 +4,66 @@ import Loading from '../components/Loading'
 import BlurCircle from '../components/BlurCircle'
 import timeFormat from '../lib/timeFormat'
 import { dateFormat } from '../lib/dateFormat'
+import { useAppContext } from '../context/AppContext'
 
 const MyBookings = () => {
 
     const currency = import.meta.env.VITE_CURRENCY
+
+    const { navigate, axios, user, getToken, image_base_url } = useAppContext()
+
 
     const [bookings, setBookings] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
     const getMyBookings = async () => {
 
-        setBookings(dummyBookingData)
-        setIsLoading(false)
+        try {
+
+            const { data } = await axios.get("/api/user/bookings", { headers: { Authorization: `Bearer ${await getToken()}` } })
+            if (data.success) {
+                setBookings(data.bookings)
+                setIsLoading(false)
+            }
+            else {
+                toast.error(data.message)
+            }
+
+        }
+        catch (error) {
+            console.log("Error occured while reaching to the API to get the user bookings. Error : ", error)
+        }
 
     }
 
     useEffect(() => {
-        getMyBookings()
-    }, [])
+        if (user) {
+            getMyBookings()
+        }
+    }, [user])
 
     return !isLoading ? (
         <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]'>
-            <BlurCircle top="100px" left="100px"/>
+            <BlurCircle top="100px" left="100px" />
             <div>
-                <BlurCircle top="0px" left="600px" />
+                <BlurCircle bottom="0px" left="600px" />
             </div>
             <h1 className='text-lg font-semibold mb-4'>My Bookings</h1>
+
+            {bookings.length === 0 &&
+                <div className='flex flex-col items-center justify-center mt-20 '>
+                    <h1 className='text-center text-3xl'>No Bookings Yet</h1>
+                    <h5 className='text-center text-lg mt-5'>Let's Book Your a Movie Show</h5>
+                    <button onClick={() => { navigate("/movies"); scrollTo(0, 0) }} className='mt-5 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Explore Movies</button>
+                </div>}
 
             {bookings.map((item, index) => (
                 <div key={index} className='flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl'>
                     <div className='flex flex-col md:flex-row'>
-                        <img src={item.show.movie.poster_path} alt="" className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded'/>
+                        <img src={image_base_url + item.show.movie.poster_path} alt="" className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded' />
                         <div className='flex flex-col p-4'>
                             <p className='text-lg font-semibold'>{item.show.movie.title}</p>
-                            <p className='text-gray-400 text-sm'>{timeFormat(item.show.movie.runtime) }</p>
+                            <p className='text-gray-400 text-sm'>{timeFormat(item.show.movie.runtime)}</p>
                             <p className='text-gray-400 text-sm mt-auto'>{dateFormat(item.show.showDateTime)}</p>
                         </div>
                     </div>
@@ -45,7 +71,7 @@ const MyBookings = () => {
                     <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
                         <div className='flex items-center gap-4'>
                             <p className='text-2xl font-semibold mb-3'>{currency}{item.amount}</p>
-                            {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>Pay Now</button> }
+                            {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>Pay Now</button>}
                         </div>
                         <div className='text-sm'>
                             <p><span className='text-gray-400'>Total Tickets : </span> {item.bookedSeats.length} </p>
@@ -53,8 +79,8 @@ const MyBookings = () => {
                         </div>
                     </div>
 
-                </div>  
-            )) }
+                </div>
+            ))}
 
         </div>
     ) : <Loading />
