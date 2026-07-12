0e73a227e61de40dc0cb5b93d9534dd6b27983af
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-12 18:21:40 +0530

    Corrected the bug of date time inconsitenst betweena dmin panel and user show

--- client/src/components/DateSelect.jsx
+++ client/src/components/DateSelect.jsx
@@ -29,12 +29,12 @@ const DateSelect = ({ dateTime, id }) => {
     return (
         <div id="dateSelect" className='pt-30'>
             <div className="flex flex-col md:flex-row items-center justify-between gap-10 relative p-8 bg-primary/10 border border-primary/20 rounded-lg">
-                <BlurCircle top="-100px" left="-100px"/>
-                <BlurCircle top="100px" right="0px"/>
+                <BlurCircle top="-100px" left="-100px" />
+                <BlurCircle top="100px" right="0px" />
                 <div>
                     <p className='text-lg font-semibold'>Choose Date</p>
                     <div className='flex items-center gap-6 text-sm mt-5'>
-                        <ChevronLeftIcon width={28}/>
+                        <ChevronLeftIcon width={28} />
                         <span className='grid grid-cols-3 md:flex flex-wrap md:max-w-lg gap-4'>
                             {Object.keys(dateTime).map((date) => {
                                 // date is in YYYY-MM-DD format (en-CA). Parse as local date to avoid UTC shifts.
@@ -49,10 +49,10 @@ const DateSelect = ({ dateTime, id }) => {
                                 )
                             })}
                         </span>
-                        <ChevronRightIcon width={28}/>
+                        <ChevronRightIcon width={28} />
                     </div>
                 </div>
-                <button onClick={onBookHandler}  className="bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer">Book Now</button>
+                <button onClick={onBookHandler} className="bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer">Book Now</button>
             </div>
         </div>
     )
